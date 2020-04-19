8cba777d9cd4cc6f217cb957567f893c2aa3681a
Matthew Harwood <harwood@uber.com>
2020-04-19 11:31:50 -0700

    added useStatic

--- src/client/components/hooks/use_cloudinary.js
+++ src/client/components/hooks/use_cloudinary.js
@@ -1,5 +1,5 @@
-export const useStatic = (path, flags) => {
-  const SECURE_BASE = "https://res.cloudinary.com/morningharwood/image/upload/";
+export const useStatic = (path, ...flags) => {
+  const SECURE_BASE = `https://res.cloudinary.com/morningharwood/image/upload/f_auto,q_auto,${flags.join(',')}/`;
   return `${SECURE_BASE}${path}`;
 }
 
--- src/client/pages/lab.js
+++ src/client/pages/lab.js
@@ -1,15 +1,12 @@
 import {html} from '../components/preact.js';
 import {PageNavigation} from '../components/page_navigation/page_navigation.js';
-// import {useStatic} from '../components/hooks/use_cloudinary.js';
+import {useStatic} from '../components/hooks/use_cloudinary.js';
 
 export const Lab = () => {
-  
+  const src = useStatic('sample.jpg', 'w_200');
   return html`
-  <div>
-    <nav>
-        <div>Morning Harwood</div>
-    </nav>  
-
-  </div>
+    <div>
+        <img src="${src}" alt="">
+    </div>
   `;
 };
